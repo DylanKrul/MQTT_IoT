296e5202b7072910ec5b07a1ea967aaa6cee2eaa
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-06-24 10:33:17 +0200

    Modified subscriber for home sensor topics

--- Subscribers/subscriber.py
+++ Subscribers/subscriber.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("Hellos/+")
+    client.subscribe("Home/#")
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
