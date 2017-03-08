3fafac155eb3a2bf73760ab20fbe7c0942591b51
LarsBergqvist <lars.g.bergqvist@gmail.com>
2017-03-08 19:27:30 +0100

    Added usage of Sensor433 library

--- RC_Transmitters/GardenClient_RC_433/GardenClient_RC_433.ino
+++ RC_Transmitters/GardenClient_RC_433/GardenClient_RC_433.ino
@@ -1,6 +1,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include "RCSwitch.h"
+#include <Sensor433.h>
 #include <Narcoleptic.h>
 
 //
@@ -9,160 +9,56 @@
 #define GARDEN_TEMP_ID 4
 #define SOIL_TEMP_ID   5
 #define TX_PIN 5
-RCSwitch transmitter = RCSwitch();
+Sensor433::Transmitter transmitter = Sensor433::Transmitter(TX_PIN);
 
-// Data wire is plugged into port 2 on the Arduino
+//
+// OneWire / DallasTemp sensor setup
+//
 #define ONE_WIRE_BUS 2
 #define TEMPERATURE_PRECISION 9
-
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
-
-// arrays to hold device addresses
-DeviceAddress insideThermometer, outsideThermometer;
+DeviceAddress soilThermometer, outsideThermometer;
 
 void setup(void)
 {
-  // start serial port
   Serial.begin(9600);
 
-  // Start up the library
   sensors.begin();
 
-  // locate devices on the bus
-  Serial.print("Locating devices...");
   Serial.print("Found ");
   Serial.print(sensors.getDeviceCount(), DEC);
   Serial.println(" devices.");
 
-  // report parasite power requirements
-  Serial.print("Parasite power is: "); 
-  if (sensors.isParasitePowerMode()) Serial.println("ON");
-  else Serial.println("OFF");
-
-  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+  if (!sensors.getAddress(soilThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
   if (!sensors.getAddress(outsideThermometer, 1)) Serial.println("Unable to find address for Device 1"); 
 
-  Serial.print("Device 0 Address: ");
-  printAddress(insideThermometer);
-  Serial.println();
-
-  Serial.print("Device 1 Address: ");
-  printAddress(outsideThermometer);
   Serial.println();
 
   // set the resolution to 9 bit
-  sensors.setResolution(insideThermometer, TEMPERATURE_PRECISION);
+  sensors.setResolution(soilThermometer, TEMPERATURE_PRECISION);
   sensors.setResolution(outsideThermometer, TEMPERATURE_PRECISION);
-
-  Serial.print("Device 0 Resolution: ");
-  Serial.print(sensors.getResolution(insideThermometer), DEC); 
-  Serial.println();
-
-  Serial.print("Device 1 Resolution: ");
-  Serial.print(sensors.getResolution(outsideThermometer), DEC); 
-  Serial.println();
-
-  transmitter.enableTransmit(TX_PIN); 
-  transmitter.setRepeatTransmit(25);
-
 }
 
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress)
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    // zero pad the address if necessary
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-  }
-}
-
-
-// function to print a device's resolution
-void printResolution(DeviceAddress deviceAddress)
-{
-  Serial.print("Resolution: ");
-  Serial.print(sensors.getResolution(deviceAddress));
-  Serial.println();    
-}
-
-
-unsigned long seqNum=0;
 void loop(void)
 { 
   sensors.requestTemperatures();
 
   float gardenTemp = sensors.getTempC(outsideThermometer);
-  float soilTemp = sensors.getTempC(insideThermometer);
+  float soilTemp = sensors.getTempC(soilThermometer);
   Serial.print("Outside temp:");
   Serial.println(gardenTemp);
   Serial.print("Soil temp:");
   Serial.println(soilTemp);
 
-  unsigned int encodedFloat = EncodeFloatToTwoBytes(gardenTemp);
-  Serial.println(encodedFloat);
-  unsigned long dataToSend = Code32BitsToSend(GARDEN_TEMP_ID,seqNum,encodedFloat);
-  TransmitWithRepeat(dataToSend);
-
-  encodedFloat = EncodeFloatToTwoBytes(soilTemp);
-  Serial.println(encodedFloat);
-  dataToSend = Code32BitsToSend(SOIL_TEMP_ID,seqNum,encodedFloat);
-  TransmitWithRepeat(dataToSend);
+  transmitter.sendFloat(GARDEN_TEMP_ID,gardenTemp);
+  transmitter.sendFloat(SOIL_TEMP_ID,soilTemp);
 
   for (int i=0; i< 100; i++)
   {
     // Max narcoleptic delay is 8s
     Narcoleptic.delay(8000);
   }
-  
-  seqNum++;
-  if (seqNum > 15)
-  {
-    seqNum = 0;
-  } 
-}
-
-unsigned long Code32BitsToSend(int measurementTypeID, unsigned long seq, unsigned long data)
-{
-    unsigned long checkSum = measurementTypeID + seq + data;
-    unsigned long byte3 = ((0x0F & measurementTypeID) << 4) + (0x0F & seq);
-    unsigned long byte2_and_byte_1 = 0xFFFF & data;
-    unsigned long byte0 = 0xFF & checkSum;
-    unsigned long dataToSend = (byte3 << 24) + (byte2_and_byte_1 << 8) + byte0;
-
-    return dataToSend;
-}
-
-// Encode a float as two bytes by multiplying with 100
-// and reserving the highest bit as a sign flag
-// Values that can be encoded correctly are between -327,67 and +327,67
-unsigned int EncodeFloatToTwoBytes(float floatValue)
-{
-  bool sign = false;
-  
-  if (floatValue < 0) 
-    sign=true;  
-      
-  int integer = (100*fabs(floatValue));
-  unsigned int word = integer & 0XFFFF;
-  
-  if (sign)
-    word |= 1 << 15;
-
-  return word;
-}
-
-void TransmitWithRepeat(unsigned long dataToSend)
-{
-    transmitter.send(dataToSend, 32);
-    Narcoleptic.delay(2000);
-    transmitter.send(dataToSend, 32);
-    Narcoleptic.delay(2000);
 }
 
 
--- RC_Transmitters/TopFloorClient_RC_433/TopFloorClient_RC_433.ino
+++ RC_Transmitters/TopFloorClient_RC_433/TopFloorClient_RC_433.ino
@@ -7,11 +7,8 @@
 
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
-#include "RCSwitch.h"
+#include <Sensor433.h>
 #include <Narcoleptic.h>
-
-#define CLIENT_NAME "TopFloorClient"
-#define TX_PIN 10                     // PWM output pin to use for transmission
 
 //
 // Sensor setup
@@ -22,22 +19,19 @@ Adafruit_BMP085 bmp; // pin 4, SDA (data), pin 5, SDC (clock)
 //
 // Data transmission setup
 //
+#define CLIENT_NAME "TopFloorClient"
+#define TX_PIN 12                     // PWM output pin to use for transmission
 #define TOPFLOOR_TEMP_ID    1
 #define BMP_PRESSURE_ID     2
-RCSwitch transmitter = RCSwitch();
+Sensor433::Transmitter transmitter = Sensor433::Transmitter(TX_PIN);
 
-#define SLEEP_SECONDS 60*15
 void setup() 
 {
   Serial.begin(9600);
   
   bmp.begin();
-
-  transmitter.enableTransmit(TX_PIN); 
-  transmitter.setRepeatTransmit(25);
 }
 
-unsigned long seqNum=0;
 void loop() 
 {
   float temp = bmp.readTemperature();
@@ -46,9 +40,7 @@ void loop()
   Serial.print(temp);
   Serial.println(" *C");
 
-  unsigned int encodedFloat = EncodeFloatToTwoBytes(temp);
-  unsigned long dataToSend = Code32BitsToSend(TOPFLOOR_TEMP_ID,seqNum,encodedFloat);
-  TransmitWithRepeat(dataToSend);
+  transmitter.sendFloat(TOPFLOOR_TEMP_ID, temp);
 
   float pressure = bmp.readPressure();
   Serial.print("Pressure = ");
@@ -58,54 +50,12 @@ void loop()
   unsigned int pressureInt = pressure/100;
   Serial.print("Pressure Int = ");
   Serial.println(pressureInt);
-  dataToSend = Code32BitsToSend(BMP_PRESSURE_ID,seqNum,pressureInt);
-  TransmitWithRepeat(dataToSend);
-
-  Narcoleptic.delay(SLEEP_SECONDS*1000);
-
-  seqNum++;
-  if (seqNum > 15)
+  transmitter.sendWord(BMP_PRESSURE_ID, pressureInt);
+  
+  for (int i=0; i< 100; i++)
   {
-    seqNum = 0;
-  } 
-}
-
-
-unsigned long Code32BitsToSend(int measurementTypeID, unsigned long seq, unsigned long data)
-{
-    unsigned long checkSum = measurementTypeID + seq + data;
-    unsigned long byte3 = ((0x0F & measurementTypeID) << 4) + (0x0F & seq);
-    unsigned long byte2_and_byte_1 = 0xFFFF & data;
-    unsigned long byte0 = 0xFF & checkSum;
-    unsigned long dataToSend = (byte3 << 24) + (byte2_and_byte_1 << 8) + byte0;
-
-    return dataToSend;
-}
-
-// Encode a float as two bytes by multiplying with 100
-// and reserving the highest bit as a sign flag
-// Values that can be encoded correctly are between -327,67 and +327,67
-unsigned int EncodeFloatToTwoBytes(float floatValue)
-{
-  bool sign = false;
-  
-  if (floatValue < 0) 
-    sign=true;  
-      
-  int integer = (100*fabs(floatValue));
-  unsigned int word = integer & 0XFFFF;
-  
-  if (sign)
-    word |= 1 << 15;
-
-  return word;
-}
-
-void TransmitWithRepeat(unsigned long dataToSend)
-{
-    transmitter.send(dataToSend, 32);
-    Narcoleptic.delay(2000);
-    transmitter.send(dataToSend, 32);
-    Narcoleptic.delay(2000);
+    // Max narcoleptic delay is 8s
+    Narcoleptic.delay(8000);
+  }
 }
 
